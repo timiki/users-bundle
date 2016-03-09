1ecbf1e73b1be47a2fdca395a5a8dad1de68fd28
nikita <nikita@netesin.ru>
2016-03-09 16:22:54 +0800

    Обновление timiki/rpc-client-common до 1.4.0

--- Security/Providers/RpcProvider.php
+++ Security/Providers/RpcProvider.php
@@ -2,11 +2,12 @@
 
 namespace Timiki\Bundle\UsersBundle\Security\Providers;
 
-use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use \Symfony\Component\HttpFoundation\Request;
 use Timiki\Bundle\UsersBundle\Models\User;
 use Timiki\RpcClientCommon\Client;
 use DateTime;
@@ -17,168 +18,182 @@ use DateTime;
 class RpcProvider implements UserProviderInterface
 {
 
-    /**
-     * @var Container
-     */
-    protected $container;
+	/**
+	 * @var ContainerInterface
+	 */
+	protected $container;
 
-    /**
-     * @var Client
-     */
-    protected $client;
+	/**
+	 * @var Client
+	 */
+	protected $client;
 
-    /**
-     * Create new rpc users providers
-     *
-     * @param array     $options
-     * @param Container $container
-     */
-    public function __construct(array $options, Container $container)
-    {
-        // Create RPC client
+	/**
+	 * Create new rpc users providers
+	 *
+	 * @param array $options
+	 * @param ContainerInterface $container
+	 */
+	public function __construct(array $options, ContainerInterface $container)
+	{
+		// Create RPC client
 
-        if (array_key_exists('type', $options)) {
-            $type = $options['type'];
-        } else {
-            $type = 'json';
-        }
+		if (array_key_exists('type', $options)) {
+			$type = $options['type'];
+		} else {
+			$type = 'json';
+		}
 
-        if (array_key_exists('address', $options)) {
-            $address = $options['address'];
-        } else {
-            $address = null;
-        }
+		if (array_key_exists('address', $options)) {
+			$address = $options['address'];
+		} else {
+			$address = null;
+		}
 
-        if (array_key_exists('options', $options)) {
-            $options = $options['options'];
-        } else {
-            $options = [];
-        }
+		$headers = (array)$options['headers'];
+		$cookies = (array)$options['cookies'];
 
-        $this->client = new Client($address, $options, $type);
-        $this->setContainer($container);
-    }
+		if (is_array($options['forwardHeaders']) and !empty($options['forwardHeaders'])) {
+			foreach ($options['forwardHeaders'] as $header) {
+				$headers[$header] = Request::createFromGlobals()->headers->get($header);
+				if (strtolower($header) == 'client-ip') {
+					$headers[$header] = [Request::createFromGlobals()->getClientIp()];
+				}
+			}
+		}
 
-    /**
-     * Set container
-     *
-     * @param Container|null $container
-     * @return $this
-     */
-    public function setContainer(Container $container)
-    {
-        if ($container instanceof Container) {
-            $this->container = $container;
-        }
+		if (is_array($options['forwardCookies']) and !empty($options['forwardCookies'])) {
+			foreach (Request::createFromGlobals()->cookies->all() as $name => $values) {
+				if (in_array($name, $options['forwardCookies'])) {
+					$cookies[$name] = $values;
+				}
+			}
+		}
 
-        return $this;
-    }
+		$this->client = new Client($address, $type, $headers, $cookies);
+		$this->setContainer($container);
+	}
 
-    /**
-     * Get container
-     *
-     * @return Container|null
-     */
-    public function getContainer()
-    {
-        return $this->container;
-    }
+	/**
+	 * Set container
+	 *
+	 * @param ContainerInterface|null $container
+	 * @return $this
+	 */
+	public function setContainer(ContainerInterface $container)
+	{
+		if ($container instanceof ContainerInterface) {
+			$this->container = $container;
+		}
 
-    /**
-     * Get rpx client
-     *
-     * @return Client|null
-     */
-    public function getClient()
-    {
-        return $this->client;
-    }
+		return $this;
+	}
 
-    /**
-     * Loads the user for the given username.
-     *
-     * This method must throw UsernameNotFoundException if the user is not
-     * found.
-     *
-     * @param string $username The username
-     *
-     * @return UserInterface
-     *
-     * @see UsernameNotFoundException
-     *
-     * @throws UsernameNotFoundException if the user is not found
-     */
-    public function loadUserByUsername($username)
-    {
-        if ($client = $this->getClient()) {
+	/**
+	 * Get container
+	 *
+	 * @return ContainerInterface|null
+	 */
+	public function getContainer()
+	{
+		return $this->container;
+	}
 
-            // Before run call need stop session
-            if ($this->getContainer() !== null) {
-                $this->getContainer()->get('session')->save();
-            }
+	/**
+	 * Get rpx client
+	 *
+	 * @return Client|null
+	 */
+	public function getClient()
+	{
+		return $this->client;
+	}
 
-            $result = $client->call('UserByUsername', ['username' => $username]);
+	/**
+	 * Loads the user for the given username.
+	 *
+	 * This method must throw UsernameNotFoundException if the user is not
+	 * found.
+	 *
+	 * @param string $username The username
+	 *
+	 * @return UserInterface
+	 *
+	 * @see UsernameNotFoundException
+	 *
+	 * @throws UsernameNotFoundException if the user is not found
+	 */
+	public function loadUserByUsername($username)
+	{
+		if ($client = $this->getClient()) {
 
-            // After run call need restart session
-            if ($this->getContainer() !== null) {
-                $this->getContainer()->get('session')->migrate();
-            }
+			// Before run call need stop session
+			if ($this->getContainer() !== null) {
+				$this->getContainer()->get('session')->save();
+			}
 
-            if (empty($result->getResult()->error)) {
-                // Get data from rpc response
-                $data = (array)$result->getResult()->result->data;
-                // Ro DateTime
-                if (array_key_exists('created_at', $data)) {
-                    $data['created_at'] = DateTime::createFromFormat('Y-m-d H:i:s', $data['created_at']);
-                }
-                if (array_key_exists('expired_at', $data)) {
-                    $data['expired_at'] = DateTime::createFromFormat('Y-m-d H:i:s', $data['expired_at']);
-                }
-                if (array_key_exists('password_expired_at', $data)) {
-                    $data['password_expired_at'] = DateTime::createFromFormat('Y-m-d H:i:s', $data['password_expired_at']);
-                }
-                if (array_key_exists('last_login_at', $data)) {
-                    $data['last_login_at'] = DateTime::createFromFormat('Y-m-d H:i:s', $data['last_login_at']);
-                }
+			$result = $client->call('UserByUsername', ['username' => $username]);
 
-                return new User($data);
-            } else {
-                // Error call RPC
-                throw new UnsupportedUserException();
-            }
-        }
+			// After run call need restart session
+			if ($this->getContainer() !== null) {
+				$this->getContainer()->get('session')->migrate();
+			}
 
-        throw new UsernameNotFoundException();
-    }
+			if (empty($result->getResult()->error)) {
+				// Get data from rpc response
+				$data = (array)$result->getResult()->result->data;
+				// Ro DateTime
+				if (array_key_exists('created_at', $data)) {
+					$data['created_at'] = DateTime::createFromFormat('Y-m-d H:i:s', $data['created_at']);
+				}
+				if (array_key_exists('expired_at', $data)) {
+					$data['expired_at'] = DateTime::createFromFormat('Y-m-d H:i:s', $data['expired_at']);
+				}
+				if (array_key_exists('password_expired_at', $data)) {
+					$data['password_expired_at'] = DateTime::createFromFormat('Y-m-d H:i:s', $data['password_expired_at']);
+				}
+				if (array_key_exists('last_login_at', $data)) {
+					$data['last_login_at'] = DateTime::createFromFormat('Y-m-d H:i:s', $data['last_login_at']);
+				}
 
-    /**
-     * Refreshes the user for the account interface.
-     *
-     * It is up to the implementation to decide if the user data should be
-     * totally reloaded (e.g. from the database), or if the UserInterface
-     * object can just be merged into some internal array of users / identity
-     * map.
-     *
-     * @param UserInterface $user
-     *
-     * @return UserInterface
-     *
-     * @throws UnsupportedUserException if the account is not supported
-     */
-    public function refreshUser(UserInterface $user)
-    {
-        return $this->loadUserByUsername($user->getUsername());
-    }
+				return new User($data);
+			} else {
+				// Error call RPC
+				throw new UnsupportedUserException();
+			}
+		}
 
-    /**
-     * Whether this provider supports the given user class.
-     *
-     * @param string $class
-     *
-     * @return bool
-     */
-    public function supportsClass($class)
-    {
-        return $class === 'Timiki\Bundle\UsersBundle\Models\User';
-    }
+		throw new UsernameNotFoundException();
+	}
+
+	/**
+	 * Refreshes the user for the account interface.
+	 *
+	 * It is up to the implementation to decide if the user data should be
+	 * totally reloaded (e.g. from the database), or if the UserInterface
+	 * object can just be merged into some internal array of users / identity
+	 * map.
+	 *
+	 * @param UserInterface $user
+	 *
+	 * @return UserInterface
+	 *
+	 * @throws UnsupportedUserException if the account is not supported
+	 */
+	public function refreshUser(UserInterface $user)
+	{
+		return $this->loadUserByUsername($user->getUsername());
+	}
+
+	/**
+	 * Whether this provider supports the given user class.
+	 *
+	 * @param string $class
+	 *
+	 * @return bool
+	 */
+	public function supportsClass($class)
+	{
+		return $class === 'Timiki\Bundle\UsersBundle\Models\User';
+	}
 }
